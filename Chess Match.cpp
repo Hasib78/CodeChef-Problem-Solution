70a05bdc796da836e6362c81b15124b1d26fec41
Shaikh Hasibul Islam <islam.hasib78657@gmail.com>
2022-11-03 14:02:34 +0600

    Add files via upload

---
+++ Chess Match.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n,a,b,x;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>a>>b;
+        x=(2*(180+n))-(a+b);
+        cout<<x<<endl;
+
+    }
+}
